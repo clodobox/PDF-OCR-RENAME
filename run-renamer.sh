7ce821cfbd68a127e7bc9ea5332c5646c1202468
Julien Lanza <julien.lanza@ik.me>
2023-04-27 12:27:30 +0300

    CMD changes

--- run-renamer.sh
+++ run-renamer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # echo 'Renamer started !'
-python3 RenameMyPDF.py
+python3 -u RenameMyPDF.py
--- run-watcher.sh
+++ run-watcher.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'Watcher started !'
-python3 watcher.py
+python3 -u watcher.py
